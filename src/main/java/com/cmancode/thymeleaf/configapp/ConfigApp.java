7c13e0fd2b865e21bed52f65385d0fd897c96e9a
Carlos Alberto <carmantillab@gmail.com>
2017-12-03 19:20:20 -0500

    Thymeleaf configurado

--- src/main/java/com/cmancode/thymeleaf/configapp/ConfigApp.java
+++ src/main/java/com/cmancode/thymeleaf/configapp/ConfigApp.java
@@ -3,26 +3,64 @@ package com.cmancode.thymeleaf.configapp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = {"com.cmancode.thymeleaf"})
-//@Import(value = { SecurityConfig.class }) En caso de aplicar Spring Security
+//@Import(value = { SecurityConfig.class }) 
 public class ConfigApp extends WebMvcConfigurerAdapter{
 	
 	@Bean
-	public InternalResourceViewResolver viewResolver() {
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+	@Description("Thymeleaf template resolver")
+	public ServletContextTemplateResolver templateResolver() {
+		
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/views/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		
+		/*InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
 		viewResolver.setViewClass(JstlView.class);
 		viewResolver.setPrefix("/WEB-INF/views/");
-		viewResolver.setSuffix(".jsp");
-		return viewResolver;
+		viewResolver.setSuffix(".jsp");*/
+		return templateResolver;
+	}
+	
+	@Bean
+	@Description("Thymeleaf Template Engine")
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		templateEngine.setTemplateEngineMessageSource(messageSource());
+		return templateEngine;
+	}
+	
+	@Bean
+	@Description("Thymeleaf View Resolver")
+	public ThymeleafViewResolver viewResolver() {
+	    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+	    viewResolver.setTemplateEngine(templateEngine());
+	    viewResolver.setOrder(1);
+	    return viewResolver;
+	}
+	
+	@Bean
+	@Description("Spring Message Resolver")
+	public ResourceBundleMessageSource messageSource() {
+	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	    messageSource.setBasename("messages");
+	    return messageSource;
 	}
 	
 	@Override
